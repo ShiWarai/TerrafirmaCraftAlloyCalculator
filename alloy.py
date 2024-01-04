46faafa853854e4aa5b672387f2ccaee78232bef
ShiWarai <zhuravlev.p.r@edu.mirea.ru>
2024-01-04 10:51:53 +0300

    Feat: add searching of best unit combination

--- alloy.py
+++ alloy.py
@@ -7,12 +7,12 @@ class Alloy:
     def __init__(self, name: str = None, composition: Dict[str, tuple[int, int]] = {}):
         self.name = name
         self.composition = composition
-        self.__material_quantity = len(list(self.composition.keys()))
+        self.material_quantity = len(list(self.composition.keys()))
 
     def get_minimal_proportion(self, materials_unit: list[int], req_quantity: int) -> list[int]:
         # Получение первичной пропорции (по минимальной границе)
         composition_values = list(self.composition.values())
-        proportion: list[int] = [math.ceil((composition_values[i][0] / 100 * req_quantity) / materials_unit[i]) for i in range(self.__material_quantity)]
+        proportion: list[int] = [math.ceil((composition_values[i][0] / 100 * req_quantity) / materials_unit[i]) for i in range(self.material_quantity)]
 
         while True:
             current_sum, potentially_incr_index = self.get_potentially_increasing_material(proportion, materials_unit)
@@ -25,22 +25,22 @@ class Alloy:
         return proportion
 
     def get_potentially_increasing_material(self, current_proportion: list[int], materials_unit: list[int]) -> tuple[int, int]:
-        current_materials_quantities = [current_proportion[i] * materials_unit[i] for i in range(self.__material_quantity)]
+        current_materials_quantities = [current_proportion[i] * materials_unit[i] for i in range(self.material_quantity)]
         current_sum = sum(current_materials_quantities)
         composition_values = list(self.composition.values())
 
         calculated_coefficients: list[float] = list()
-        for i in range(self.__material_quantity):
+        for i in range(self.material_quantity):
             calculated_coefficients.append((composition_values[i][1] - 100 * current_materials_quantities[i] / current_sum) / materials_unit[i])
 
         return current_sum, calculated_coefficients.index(max(calculated_coefficients))
 
     def validate_proportion(self, current_proportion: list[int], materials_unit: list[int]) -> bool:
-        current_materials_quantities = [current_proportion[i] * materials_unit[i] for i in range(self.__material_quantity)]
+        current_materials_quantities = [current_proportion[i] * materials_unit[i] for i in range(self.material_quantity)]
         current_sum = sum(current_materials_quantities)
         composition_values = list(self.composition.values())
 
-        for i in range(self.__material_quantity):
+        for i in range(self.material_quantity):
             if (composition_values[i][0] > (100 * current_materials_quantities[i] / current_sum)
                     or composition_values[i][1] < (100 * current_materials_quantities[i] / current_sum)):
                 return False
--- main.py
+++ main.py
@@ -1,41 +1,97 @@
+import math
 from menu_functions import calculate_materials_quantity, Alloy
 from alloy import BUILTIN_ALLOYS
 from os import system
 from morph import decline_noun_by_number
+from itertools import permutations, product
+
+from typing import Dict
 
 
-def menu_selector(mode: int):
-    materials_unit: list[int] = list()
-    match mode:
-        case 1:
-            alloy_name = input("Введите имя сплава: ")
+class Menu:
+    @classmethod
+    def menu_selector(cls, mode: list[int]):
+        match mode[1]:
+            case 1:
+                introduced_alloy = cls.input_alloy(mode[0])
+                materials_units = cls.input_materials_units(introduced_alloy)
+                alloy_quantity = cls.input_alloy_quantity()
 
-            alloy_composition = dict(); material_index = 0
-            while True:
-                material_index += 1
-                material_name = input(f"\n%d) Введите имя материала (или ничего, если состав введён до конца): " % material_index)
-                if material_name == "" or not material_name:
-                    break
+                material_proportions = calculate_materials_quantity(introduced_alloy, materials_units, alloy_quantity)
 
-                min_percentage, max_percentage = map(float, input("Диапазон % содержания в сплаве (через пробел): ").split())
-                alloy_composition[material_name] = (min_percentage, max_percentage)
+                # Вывод данных
+                print("\n" + 50 * "-")
+                cls.print_proportion(introduced_alloy, materials_units, material_proportions)
+            case 2:
+                introduced_alloy = cls.input_alloy(mode[0])
+                alloy_quantity = cls.input_alloy_quantity()
 
-            introduced_alloy: Alloy = Alloy(alloy_name, alloy_composition)
-        case 2:
-            # Выбор сплава из заранее созданных
-            print("Встроенные сплавы:")
-            for i in range(len(BUILTIN_ALLOYS)):
-                print(f"%d) %s" % (i+1, BUILTIN_ALLOYS[i].name))
+                best_proportion: tuple[Dict[str, int], list[int], int] = (None, None, math.inf)
+                unit_types = [1, 2, 3, 4]
+                for permutation in list(product(unit_types, repeat=len(unit_types))):
+                    materials_units: list[int] = list(map(cls.get_material_unit_by_type, permutation))
 
-            introduced_alloy: Alloy = BUILTIN_ALLOYS[int(input("> ")) - 1]
-        case _:
-            return
+                    material_proportions = calculate_materials_quantity(introduced_alloy, materials_units, alloy_quantity)
+                    materials_sum = sum(list(material_proportions.values()))
+                    if best_proportion[2] > materials_sum:
+                        best_proportion = (material_proportions, materials_units, materials_sum)
 
-    # Размер кусков металлов, которые будут использоваться в плавке
-    print("Размер кусков материала (1 - богатый, 2 - обычный, 3 - бедный, 4 - кусочек, остальные согласно введённому кол-ву с клавиатуры):")
-    for material_name in list(introduced_alloy.composition.keys()):
-        unit = int(input(f"%s: " % material_name))
-        match unit:
+                print("\n" + 50 * "-")
+                print("\nЛучший вариант с размерами кусков:", end='')
+                for material in list(introduced_alloy.composition.keys()):
+                    print(" '%s': %d %s" % (material, best_proportion[1][list(introduced_alloy.composition.keys()).index(material)], "мВ"), end=';')
+                print('')
+
+                cls.print_proportion(introduced_alloy, best_proportion[1], best_proportion[0])
+            case _:
+                return
+
+    @classmethod
+    def input_alloy(cls, input_type: int) -> Alloy:
+        match input_type:
+            case 1:
+                alloy_name = input("\nВведите имя сплава: ")
+
+                alloy_composition = dict()
+                material_index = 0
+                while True:
+                    material_index += 1
+                    material_name = input(
+                        f"\n%d) Введите имя материала (или ничего, если состав введён до конца): " % material_index)
+                    if material_name == "" or not material_name:
+                        break
+
+                    min_percentage, max_percentage = map(float, input(
+                        "Диапазон % содержания в сплаве (через пробел): ").split())
+                    alloy_composition[material_name] = (min_percentage, max_percentage)
+
+                return Alloy(alloy_name, alloy_composition)
+            case 2:
+                # Выбор сплава из заранее созданных
+                print("\nВстроенные сплавы:")
+                for i in range(len(BUILTIN_ALLOYS)):
+                    print(f"%d) %s" % (i + 1, BUILTIN_ALLOYS[i].name))
+
+                return BUILTIN_ALLOYS[int(input("> ")) - 1]
+            case _:
+                return None
+
+    @classmethod
+    def input_materials_units(cls, alloy: Alloy):
+        materials_units: list[int] = list()
+
+        # Размер кусков металлов, которые будут использоваться в плавке
+        print("Размер кусков материала (1 - богатый, 2 - обычный, 3 - бедный, 4 - кусочек, остальные согласно введённому кол-ву с клавиатуры):")
+        for material_name in list(alloy.composition.keys()):
+            unit = cls.get_material_unit_by_type(int(input(f"%s: " % material_name)))
+            materials_units.append(unit)
+
+        return materials_units
+
+    @classmethod
+    def get_material_unit_by_type(cls, unit_type: int) -> int:
+        unit = 0
+        match unit_type:
             case 1:
                 unit = 35
             case 2:
@@ -44,33 +100,46 @@ def menu_selector(mode: int):
                 unit = 15
             case 4:
                 unit = 10
-            case _:
-                pass
-        materials_unit.append(unit)
 
-    # Кол-во сплава, для которого надо рассчитать пропорцию
-    alloy_quantity: int = int(input("Требуемый минимальный объём сплава (в мВ): "))
-    assert alloy_quantity > 0
+        return unit
 
-    material_proportions = calculate_materials_quantity(introduced_alloy, materials_unit, alloy_quantity)
+    @classmethod
+    def input_alloy_quantity(cls):
+        alloy_quantity: int = int(input("Требуемый минимальный объём сплава (в мВ): "))
+        assert alloy_quantity > 0
 
-    # Вывод данных
-    materials_names = list(material_proportions.keys())
-    print(f"\nВариант пропорции для получения сплава %s в объёме %d %s:" % (introduced_alloy, sum(list(material_proportions.values())), 'мВ'))
-    for material in materials_names:
-        print(f"Металл \"%s\": %d %s" % (material, material_proportions[material], "мВ"), end='')
-        material_unit = materials_unit[list(introduced_alloy.composition.keys()).index(material)]
-        print(" или %s" % decline_noun_by_number(int(material_proportions[material]/material_unit), "кусок"))
+        return alloy_quantity
+
+    @classmethod
+    def print_proportion(cls, alloy: Alloy, materials_unit: list[int], material_proportions: Dict[str, int]):
+        materials_names = list(material_proportions.keys())
+
+        print(f"\nВариант пропорции для получения сплава %s в объёме %d %s:" % (
+            alloy, sum(list(material_proportions.values())), 'мВ'))
+        for material in materials_names:
+            print(f"Металл \"%s\": %d %s" % (material, material_proportions[material], "мВ"), end='')
+            material_unit = materials_unit[list(alloy.composition.keys()).index(material)]
+            print(" или %s" % decline_noun_by_number(int(material_proportions[material] / material_unit), "кусок"))
 
 
 if __name__ == '__main__':
     while True:
-        result: int = int(input('1) Посчитать для нового сплава\n2) Посчитать для встроенного сплава\n0) Выйти\n> '))
+        result: list[int] = list()
 
-        if result == 0:
+        result.append(int(input('''1) Посчитать для нового сплава\n'''
+                                '''2) Посчитать для встроенного сплава\n'''
+                                '''0) Выйти\n> ''')))
+        if result[0] == 0:
             break
-        else:
-            menu_selector(result)
 
-            input("\n" + 50 * "-" + "\n")
-            system('cls')
+        result.append(int(input('''1) Для фиксированных кусков металла\n'''
+                                '''2) Подобрать с эффективным набором кусков металла\n'''
+                                '''0) Выйти\n> ''')))
+        if result[1] == 0:
+            break
+
+        # Запуск селектора режимов
+        Menu.menu_selector(result)
+
+        input("\n" + 50 * "-" + "\n")
+        system('cls')
